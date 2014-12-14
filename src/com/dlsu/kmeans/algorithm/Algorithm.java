c56582ce31472048e9664dd937a16307007550bb
Mike Dayupay <mike.dayupay03@gmail.com>
2014-12-14 17:43:23 +0800

    initializing the clusters and adding initial members to clusters

--- src/com/dlsu/kmeans/algorithm/Algorithm.java
+++ src/com/dlsu/kmeans/algorithm/Algorithm.java
@@ -9,9 +9,10 @@ import com.dlsu.kmeans.object.Music;
 
 public class Algorithm {
 	
-	private int euclideanCompCount;
-	private int pass = 100;
+	private int euclideanCompCount = 0;
+	private int pass = 0;
 	private int k = 10;
+	private double minValue = Double.MAX_VALUE;
 	private ArrayList<Music> musicSample = new ArrayList<Music>();
 	private Cluster[] clusters = new Cluster[k];
 	
@@ -19,15 +20,66 @@ public class Algorithm {
 		Import importFile = new Import();
 		musicSample.addAll(importFile.performImport());
 		
+		initializeClusters();
+		for(int i = 0; i < clusters[1].getClusterMembers().size(); i++)
+			System.out.println(clusters[1].getClusterMembers().get(i).getMusicName());
+		do {
+			
+			for(int a = 0; a < clusters.length; a++) {
+				clusters[a].setClusterCentroid();
+			}
+			
+			
+			for(int i = 0 ; i < musicSample.size(); i++) {
+				for(int j = 0; j < clusters.length; j++) {
+					musicSample.get(i).setDistanceFromCentroid(clusters[j].getClusterCentroid());
+					
+					if(musicSample.get(i).getDistanceFromCentroid() < minValue) {
+						minValue = musicSample.get(i).getDistanceFromCentroid();
+							
+						for(int k = 0; k < clusters.length; k ++) {
+							if(clusters[k].getClusterMembers().contains(musicSample.get(i))) {
+								clusters[k].getClusterMembers().remove(musicSample.get(i));
+							}
+						}
+						clusters[j].insertClusterMembers(musicSample.get(i));
+					}
+				}
+			}	
+			
+			pass++;
+			
+		}while (pass < 100);
+		
+		
+		
 		
 	}
 	
 	public void initializeClusters() {
 		Random rand = new Random();
-		int rnd = rand.nextInt(1000);
-		for(int i = 0; i <= k; i++) {
-			clusters[i] = new Cluster("A", musicSample.get(rnd).getMusicAttr(), musicSample.get(rnd));
+		int flag = 0;
+		
+		
+		for(int i = 0; i < k; i++) {
+			int rnd = rand.nextInt(1000);
+			clusters[i] = new Cluster(i+"", musicSample.get(rnd).getMusicAttr(), musicSample.get(rnd));
 		}
+		
+		for(int a = 0 ; a < musicSample.size(); a++) {
+			Double min = null;
+			for(int b = 0; b < clusters.length; b++) {
+				musicSample.get(a).setDistanceFromCentroid(clusters[b].getClusterCentroid());
+			
+				if(min == null || musicSample.get(a).getDistanceFromCentroid() < min) {
+					min = musicSample.get(a).getDistanceFromCentroid();
+					flag = b;
+					System.out.println(flag);
+				}
+			}
+			clusters[flag].insertClusterMembers(musicSample.get(a));
+		}
+		
 	}
 	
 	
